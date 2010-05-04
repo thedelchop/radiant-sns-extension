2737e4b383e0d0da2226e53e458f375485519c6d
Jim Gay <jim@saturnflyer.com>
2010-05-04 10:42:06 -0400

    designer role access

--- app/controllers/admin/text_assets_controller.rb
+++ app/controllers/admin/text_assets_controller.rb
@@ -1,7 +1,7 @@
 class Admin::TextAssetsController < Admin::ResourceController
 
   only_allow_access_to :index, :new, :edit, :remove, :upload,
-    :when => [:developer, :admin],
+    :when => [:designer, :admin],
     :denied_url => { :controller => 'pages', :action => 'index' },
     :denied_message => 'You must have developer or administrator privileges to perform this action.'
 
